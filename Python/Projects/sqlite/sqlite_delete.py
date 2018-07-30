ff62adb7cf2d09b1ee8e53b2f6f3cb646ab63c34
vaishnavi-consultants <41066369+vaishnavi-consultants@users.noreply.github.com>
2018-07-30 20:25:37 +0530

    Add files via upload

--- Python/Projects/sqlite/sqlite_delete.py
+++ Python/Projects/sqlite/sqlite_delete.py
@@ -1,16 +1,21 @@
 # code for delete operation
 import sqlite3
+from sqlite_fetch import sqlite3_fetch_rec
+
+def sqlite3_delete_rec(dbname, tablename, condi):
  
-# database name to be passed as parameter
-conn = sqlite3.connect('myTable.db')
+   # database name to be passed as parameter
+   db_name = dbname+".db"
+   conn = sqlite3.connect(db_name)
  
-# delete student record from database
-conn.execute("DELETE from emp where staff_number='1'")
-conn.commit()
-print ("Total number of rows deleted :", conn.total_changes)
- 
-cursor = conn.execute("SELECT * FROM emp")
-for row in cursor:
-   print (row)
- 
-conn.close()
+   # delete student record from database
+   sql_command = """DELETE from """ + tablename + """ where """+ condi
+   conn.execute(sql_command)
+   conn.commit()
+   print ("Total number of rows deleted :", conn.total_changes)
+
+   # fetch and display the db records
+   sqlite3_fetch_rec(dbname, tablename)
+   
+   # close the connection
+   conn.close()

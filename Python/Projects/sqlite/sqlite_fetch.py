b6ffcea9cdf78e761272c49135a868b5f67c8998
vaishnavi-consultants <41066369+vaishnavi-consultants@users.noreply.github.com>
2018-07-30 15:56:35 +0530

    Add files via upload

--- Python/Projects/sqlite/sqlite_fetch.py
+++ Python/Projects/sqlite/sqlite_fetch.py
@@ -2,19 +2,22 @@
  
 # importing the module
 import sqlite3
+
+def sqlite3_fetch_rec(dbname, tablename):
  
-# connect withe the myTable database
-connection = sqlite3.connect("myTable.db")
+    # connect withe the myTable database
+    db_name = dbname+".db"
+    connection = sqlite3.connect(db_name)
  
-# cursor object
-crsr = connection.cursor()
+    # cursor object
+    crsr = connection.cursor()
  
-# execute the command to fetch all the data from the table emp
-crsr.execute("SELECT * FROM emp") 
+    # execute the command to fetch all the data from the table emp
+    crsr.execute("SELECT * FROM "+ tablename) 
  
-# store all the fetched data in the ans variable
-ans= crsr.fetchall() 
+    # store all the fetched data in the ans variable
+    ans= crsr.fetchall() 
  
-# loop to print all the data
-for i in ans:
-    print(i)
+    # loop to print all the data
+    for i in ans:
+        print(i)

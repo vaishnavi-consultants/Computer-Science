99f6063610e35660ca231147633d9ecad6a072a7
vaishnavi-consultants <41066369+vaishnavi-consultants@users.noreply.github.com>
2018-07-30 15:38:17 +0530

    Add files via upload

--- Python/Projects/sqlite/sqlite_insert.py
+++ Python/Projects/sqlite/sqlite_insert.py
@@ -3,24 +3,23 @@
  
 # importing module
 import sqlite3
+
+def sqlite3_insert_rec(dbname, tablename, rec):
  
-# connecting to the database 
-connection = sqlite3.connect("myTable.db")
+    # connecting to the database
+    db_name = dbname+".db"
+    connection = sqlite3.connect(db_name)
  
-# cursor 
-crsr = connection.cursor() 
+    # cursor 
+    crsr = connection.cursor()   
  
-# SQL command to insert the data in the table
-sql_command = """INSERT INTO emp VALUES (23, "Rishabh", "Bansal", "M", "2014-03-28");"""
-crsr.execute(sql_command)
+    # SQL command to insert the data in the table
+    sql_command = """INSERT INTO """ + tablename + """ VALUES (""" + rec + """);"""    
+    crsr.execute(sql_command)
  
-# another SQL command to insert the data in the table
-sql_command = """INSERT INTO emp VALUES (1, "Bill", "Gates", "M", "1980-10-28");"""
-crsr.execute(sql_command)
+    # To save the changes in the files. Never skip this. 
+    # If we skip this, nothing will be saved in the database.
+    connection.commit()
  
-# To save the changes in the files. Never skip this. 
-# If we skip this, nothing will be saved in the database.
-connection.commit()
- 
-# close the connection
-connection.close()
+    # close the connection
+    connection.close()

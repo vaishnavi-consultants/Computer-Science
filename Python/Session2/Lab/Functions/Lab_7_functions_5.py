7fcd2f0721c4681f222abb079aa6df8c0329e672
vaishnavi-consultants <41066369+vaishnavi-consultants@users.noreply.github.com>
2018-08-17 18:38:42 +0530

    Add files via upload

--- Python/Session2/Lab/Functions/Lab_7_functions_5.py
+++ Python/Session2/Lab/Functions/Lab_7_functions_5.py
@@ -2,7 +2,15 @@
 # Pass by object referenc
 # Notes:
 # ------
-#passing a list to a function
+# passing a list to a function
+# Summary:
+# --------
+# In Python, values are passed to functions by object references.
+# If the object is immutable, the modified value is not available outside
+# the function and if the object is mutable, its modified version is available
+# outside the function.
+# Please observe the id value before and after function call.
+
 def modify(lst):
     """ to add a new element to list """
     lst.append(9)

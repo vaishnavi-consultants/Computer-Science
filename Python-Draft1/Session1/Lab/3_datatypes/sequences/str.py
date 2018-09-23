364fffafdac4c672dd0c01235b27d9ec249073db
vaishnavi-consultants <41066369+vaishnavi-consultants@users.noreply.github.com>
2018-09-23 17:25:48 +0530

    Add files via upload

--- Python-Draft1/Session1/Lab/3_datatypes/sequences/str.py
+++ Python-Draft1/Session1/Lab/3_datatypes/sequences/str.py
@@ -1,12 +1,9 @@
 # Sequences
 
 #   str
-#   bytes
-#   bytearray
-#   list
-#   tuple
-#   range
-
+print("*" * 15)
+print("Case1: String representation")
+print("*" * 15)
 # str - String representation
 str1 = "Welcome to Python Training"
 print(str1)
@@ -28,6 +25,37 @@ print(str4)
 #  This line also spans
 # over multiline
 
+print("*" * 15)
+print("Case 2: String Iteration")
+print("*" * 15)
+SampleS = "Welcome"
+# Iterating thorugh the string and printing
+for c in SampleS:
+    print(c, end=' ')
+
+print("")
+
+print("*" * 15)
+print("Case 3: in operator")
+print("*" * 15)
+# in operator
+if "k" in SampleS:
+    print("k found in SampleS")
+else:
+    print("k not found in SampleS")
+
+# in operator
+if "W" in SampleS:
+    print("W found in SampleS")
+else:
+    print("W not found in SampleS")
+
+# Substring search
+print("searching 'come' in Welcome")
+print('come' in SampleS)
+print("*" * 15)
+print("Case 4: slice/indexing operator")
+print("*" * 15)
 # str - slicing
 str1 = "Welcome"
 str2 = "Python"
@@ -45,9 +73,14 @@ print(str1[-1])
 
 print(str1[:-1])
 # Welcom
+print("*" * 15)
+print("Case 5: Repetition")
+print("*" * 15)
 
 print(str1 * 2)
 #WelcomeWelcome
-
+print("*" * 15)
+print("Case 6: Concatenation")
+print("*" * 15)
 print(str1 + str2)
 # WelcomePython
